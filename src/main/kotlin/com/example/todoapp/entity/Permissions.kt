6943455bedda7fe276b8395e654b3382235d0af5
subalesh <subalesh.s.suresh@gmail.com>
2026-03-06 00:17:29 +0530

    create roles, todo and permissions table

---
+++ src/main/kotlin/com/example/todoapp/entity/Permissions.kt
@@ -0,0 +1,18 @@
+package com.example.todoapp.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "permissions")
+class Permissions (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+    @Column(unique = true,nullable = false)
+    val name: String,
+)
